7ba4384f8866e991427c180584bdc23c6cdc6a99
SjoerdTilmans <37220662+SjoerdTilmans@users.noreply.github.com>
2022-10-05 18:16:37 +0100

    Finilized MrBeast data collection

--- MrBeast.py
+++ MrBeast.py
@@ -3,27 +3,41 @@ import requests
 import re
 import pandas as pd
 import time
+import datetime
 
-videos = scrapetube.get_channel("UCX6OQ3DkcsbYNE6H8uQQuVA")
-data = {
-    "Title": [],
-    "Date": []
-}
+# videos = scrapetube.get_channel("UCX6OQ3DkcsbYNE6H8uQQuVA")
+# data = {
+#     "Title": [],
+#     "Date": []
+# }
+#
+# for video in videos:
+#     title = video["title"]["runs"][0]["text"]
+#     url = "https://www.youtube.com/watch?v=" + video["videoId"]
+#     r = requests.get(url)
+#
+#     page_text = r.text
+#     date = re.findall("(dateText[^0-9]+)([A-Za-z0-9 ]+)", page_text)[0][1]
+#
+#     data["Title"].append(title)
+#     data["Date"].append(date)
+#
+#     print(data)
+#
+#     df = pd.DataFrame(data=data)
+#     df.to_excel("Channel Data.xlsx")
+#
+#     time.sleep(1)
 
-for video in videos:
-    title = video["title"]["runs"][0]["text"]
-    url = "https://www.youtube.com/watch?v=" + video["videoId"]
-    r = requests.get(url)
+df = pd.read_excel("Channel Data.xlsx", index_col=0)
 
-    page_text = r.text
-    date = re.findall("(dateText[^0-9]+)([A-Za-z0-9 ]+)", page_text)[0][1]
+df["Datetime"] = df['Date'].map(lambda x: datetime.datetime.strptime(x.replace("Sept", "Sep"), "%d %b %Y"))
+df["Giveaway"] = df['Title'].map(lambda x: re.findall("\$([0-9,]+)", x.replace(",", "")) if ("$" in x and "10k" not in x) else "")
+df["Giveaway"] = df['Giveaway'].map(lambda x: float(x[0]) if (isinstance(x, list) and len(x) == 1) else 0)
+df["Giveaway Sum"] = df.loc[::-1, 'Giveaway'].cumsum()[::-1]
+df.sort_values(by='Datetime', inplace=True)
 
-    data["Title"].append(title)
-    data["Date"].append(date)
 
-    print(data)
+df.to_excel("MrBeastGivaway.xlsx")
 
-    df = pd.DataFrame(data=data)
-    df.to_excel("Channel Data.xlsx")
-
-    time.sleep(1)
+print(df)
